8496c88bdc614337c6bd1c5e6c5c00d77382b3c7
Dario Ghilardi <darioghilardi@webrain.it>
2014-07-02 15:44:02 +0200

    Using attr_reader instead of the explicit instance variable.

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,9 +1,13 @@
 class Gigasecond
-  def initialize(start)
-    @start = start.to_time
+  def initialize(start_date)
+    @start = start_date.to_time
   end
 
   def date
     (start + (10 ** 9)).to_date
   end
+
+  private
+
+  attr_reader :start
 end
