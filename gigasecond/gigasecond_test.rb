bdc73c063c2af5910c3882d83138caa97ee5cad3
Dario Ghilardi <darioghilardi@webrain.it>
2014-07-02 15:10:39 +0200

    Added my birthday

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -11,22 +11,19 @@ class GigasecondTest < MiniTest::Unit::TestCase
   end
 
   def test_2
-    skip
     gs = Gigasecond.new(Date.new(1977, 6, 13))
     assert_equal Date.new(2009, 2, 19), gs.date
   end
 
   def test_3
-    skip
     gs = Gigasecond.new(Date.new(1959, 7, 19))
     assert_equal Date.new(1991, 3, 27), gs.date
   end
 
   def test_yourself
-    skip
-    your_birthday = Date.new(year, month, day)
+    your_birthday = Date.new(1986, 5, 9)
     gs = Gigasecond.new(your_birthday)
-    assert_equal Date.new(2009, 1, 31), gs.date
+    assert_equal Date.new(2018, 1, 15), gs.date
   end
 
 end
