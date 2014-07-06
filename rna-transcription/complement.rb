5155519ebd07094d9230aa0db459c9b5a6f15703
Dario Ghilardi <darioghilardi@webrain.it>
2014-07-06 22:34:24 +0200

    rna-transcription: Usage of tr removes the need of the private translate method.

--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -2,17 +2,10 @@ class Complement
   DICTIONARY = { 'rna' => 'CGTA', 'dna' => 'GCAU' }
 
   def self.of_dna(sequence)
-    sequence.chars.map { |nucleotide| translate(nucleotide, 'rna', 'dna') }.join
+    sequence.tr(DICTIONARY['rna'], DICTIONARY['dna'])
   end
 
   def self.of_rna(sequence)
-    sequence.chars.map { |nucleotide| translate(nucleotide, 'dna', 'rna') }.join
-  end
-
-  private
-
-  def self.translate(nucleotide, source, target)
-    translation_index = DICTIONARY[source].chars.index(nucleotide)
-    DICTIONARY[target].chars[translation_index]
+    sequence.tr(DICTIONARY['dna'], DICTIONARY['rna'])
   end
 end
