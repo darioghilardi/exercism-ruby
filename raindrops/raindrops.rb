80f4b933f2a83cef9f1a4cb3dadee16211ef179f
Dario Ghilardi <darioghilardi@webrain.it>
2014-07-09 17:55:58 +0200

    Raindrops: rewritten using another algorithm.

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,17 +1,12 @@
 class Raindrops
-
-  CONVERSION = { 1 => '1', 3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
-
   def self.convert(number)
-    translation = factors(number).map { |factor| CONVERSION[factor] }.uniq.join
-    translation.empty? ? number.to_s : translation
-  end
+    conversion = ''
+    conversion << '1' if number == 1
+    conversion << 'Pling' if number % 3 == 0
+    conversion << 'Plang' if number % 5 == 0
+    conversion << 'Plong' if number % 7 == 0
 
-  private
-
-  def self.factors(n)
-    return [] if n == 1
-    factor = (2..n).detect {|x| n % x == 0}
-    [factor] + factors(n / factor)
+    return conversion if !conversion.empty?
+    number.to_s
   end
 end
